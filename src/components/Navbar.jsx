3802a681eff537b873e0df865d7054afbd10a839
Nikhil7282 <nikhil.sa2020ece@sece.ac.in>
2024-01-08 23:25:01 +0530

    initial

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,8 @@
 import React, { useState } from "react";
-import {
-  FaBars,
-  FaTimes,
-  FaGithub,
-  FaLinkedin,
-  FaFacebook,
-  FaLinkedinIn,
-} from "react-icons/fa";
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import Logo from "../assets/logo.png";
-// import { Link } from "react-router-dom";
 import { Link } from "react-scroll";
 
 const Navbar = () => {
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -14,8 +14,8 @@ const Skills = () => {
   return (
     <div name="skills" className="w-full h-screen bg-[#0a192f] text-gray-300">
       {/* Container */}
-      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
-        <div>
+      <div className="flex flex-col justify-center items-center w-full h-full">
+        <div className="simple">
           <p className="text-4xl font-bold inline border-b-4 border-pink-600 ">
             Skills
           </p>
