05dbb9574c52d294b66cd53123cc6ccbd63135bb
Nikhil7282 <nikhil.sa2020ece@sece.ac.in>
2024-01-14 19:42:08 +0530

    projects

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    // screens: {
+    //   sm: "576px",
+    // },
+    screens: {
+      xs: "576px",
+      ...defaultTheme.screens,
+    },
   },
   plugins: [],
 };
